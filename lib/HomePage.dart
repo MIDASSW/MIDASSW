9e247e3524f43d3dd0e675cc86826861cd4b31aa
Stomachyoona <dbsdksp7797@naver.com>
2024-06-09 18:08:53 +0900

    코드정리함

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:location/location.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   @override
--- lib/NearbyCrackListPage.dart
+++ lib/NearbyCrackListPage.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:intl/intl.dart';
-import 'package:flutter_crackdetectcamera/MyNearbyPage.dart';
 
 class NearbyCrackListPage extends StatefulWidget {
   @override
